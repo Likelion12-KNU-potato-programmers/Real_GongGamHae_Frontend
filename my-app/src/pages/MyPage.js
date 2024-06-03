e602bcfb6bb25ed96002f0ada622866cbd0c3861
freshman0101 <54202163+wheeze01@users.noreply.github.com>
2024-06-03 23:34:39 +0900

    Mypage - Change Profile image
    마이페이지에서 프로필 사진 변경 기능 추가

--- my-app/src/pages/MyPage.js
+++ my-app/src/pages/MyPage.js
@@ -2,8 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../components/Auth/AuthContext'; // useAuth 가져오기
 import Header from '../components/common/Header';           // header 가져오기
-import UserPosts from './UserPosts';                       // 내가 쓴 글 가져오기
-import UserCommentsPosts from './UserCommentsPosts';               // 내가 댓글 단 글 가져오기
+
 
 import '../css/MyPage.css';
 
@@ -105,6 +104,32 @@ const MyPage = () => {
         }
     };
     
+        // 프로필 이미지 업로드 처리
+        const handleImageUpload = async (event) => {
+            const imageFile = event.target.files[0]; // 선택된 이미지 파일 가져오기
+            const formData = new FormData(); // FormData 객체 생성
+            formData.append('profileImage', imageFile); // FormData에 이미지 파일 추가
+    
+            try {
+                const response = await fetch(`http://localhost:8080/api/users/me/profile`, {
+                    method: 'PUT',
+                    body: formData,
+                    credentials: 'include' // 쿠키를 포함하여 요청
+                });
+    
+              
+                
+                console.log(response.ok)
+                if (response.ok) {
+                    // 이미지 업로드 성공 시 프로필 이미지 업데이트
+                    fetchUserProfileImage(); // 이미지 업로드 후 프로필 이미지를 다시 가져와서 상태를 업데이트
+                } else {
+                    console.error('Failed to upload profile image');
+                }
+            } catch (error) {
+                console.error('Error uploading profile image:', error);
+            }
+        };
 
     // 드롭다운 Open Close 코드
     const toggleDropdown = () => {
@@ -145,6 +170,8 @@ const MyPage = () => {
                     ) : (
                         <p>Loading profile image...</p>
                     )} {/* 프로필 이미지 추가 */}
+                    <br/>
+                    <input type="file" accept="image/*" onChange={handleImageUpload} /> {/* 이미지 업로드 입력 필드 추가 */}
                     <div className="user-details">
                         <div className="nickname-container">
                             <p className="nickname">
